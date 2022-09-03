9a9475b1572bd5fc8b1d676188780d47b2d64167
Mark Ramage <mramage@hotmail.co.uk>
2022-09-03 13:41:36 +0100

    Composer updates and batch file for running unit tests on windows env

---
+++ apptests.bat
@@ -0,0 +1,20 @@
+php application.php app:build-env
+C:\laragon\bin\php\php-8.1.3-nts-Win32-vs16-x64/php ./vendor/bin/phpunit --testdox tests/Unit/CardTest.php
+
+php application.php app:build-env
+C:\laragon\bin\php\php-8.1.3-nts-Win32-vs16-x64/php ./vendor/bin/phpunit  --testdox tests/Unit/DealerTest.php --exclude skip
+
+php application.php app:build-env
+C:\laragon\bin\php\php-8.1.3-nts-Win32-vs16-x64/php ./vendor/bin/phpunit --testdox tests/Unit/DealerTest.php --filter it_can_deal_cards_to_multiple_players_at_a_table
+
+php application.php app:build-env
+C:\laragon\bin\php\php-8.1.3-nts-Win32-vs16-x64/php ./vendor/bin/phpunit tests/Unit/GamePlayTest.php
+
+php application.php app:build-env
+C:\laragon\bin\php\php-8.1.3-nts-Win32-vs16-x64/php ./vendor/bin/phpunit tests/Unit/HandIdentifierTest.php
+
+php application.php app:build-env
+C:\laragon\bin\php\php-8.1.3-nts-Win32-vs16-x64/php ./vendor/bin/phpunit tests/Unit/HandTypeTest.php
+
+php application.php app:build-env
+C:\laragon\bin\php\php-8.1.3-nts-Win32-vs16-x64/php ./vendor/bin/phpunit  tests/Unit/TableSeatTest.php
