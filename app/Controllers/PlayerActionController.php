0d2a2f7acd093c9ab7af6cc1b5a2ad6b5a721f36
Mark Ramage <mark.ramage@ebuyer.com>
2022-09-19 13:52:37 +0100

    Remove comment

--- app/Controllers/PlayerActionController.php
+++ app/Controllers/PlayerActionController.php
@@ -22,15 +22,6 @@ class PlayerActionController
             'hand_street_id' => $_POST['hand_street_id']
         ]);
 
-        /*
-         * A hacky way to resolve updated_at not changing if the action_id i the same.
-         * Issue happened when multiple rounds of re-raising takes place.
-         * 
-         * TDOD: don't think this is needed anymore now that I'm manually
-         * doing a lot of SQL. Have created a setValue method if required.
-         */
-        //$playerAction->setValue('action_id', null);
-
         $playerAction->update([
             'action_id'  => $_POST['action_id'],
             'bet_amount' => BetHelper::handle($hand, $_POST['player'], $_POST['bet_amount']),
