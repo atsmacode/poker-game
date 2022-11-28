400323376916110adc81fccd9856fd593084fdc5
Mark Ramage <mramage@hotmail.co.uk>
2022-11-28 10:32:29 +0000

    Remove var_dumps

--- config/container.php
+++ config/container.php
@@ -4,13 +4,3 @@ use Laminas\ServiceManager\ServiceManager;
 
 $dependencyMap  = require_once('dependencies.php');
 $serviceManager = new ServiceManager($dependencyMap);
-
-// $gamePlay = $serviceManager->build(GamePlay::class, [
-//     'game' => $serviceManager->get(Atsmacode\PokerGame\Classes\Game\PotLimitOmaha::class)
-// ]);
-
-// $gamePlay2 = $serviceManager->build(GamePlay::class, [
-//     'game' => $serviceManager->get(Atsmacode\PokerGame\Classes\Game\PotLimitHoldEm::class)
-// ]);
-
-// var_dump($gamePlay);
