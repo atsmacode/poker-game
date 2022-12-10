f4938d65294a516133261217e5254be4bea25a8f
Mark Ramage <mramage@hotmail.co.uk>
2022-12-10 13:42:25 +0000

    Add backslash to dependencies

--- config/dependencies.php
+++ config/dependencies.php
@@ -2,64 +2,64 @@
 
 return [
     'invokables' => [
-        Atsmacode\PokerGame\HandStep\Showdown::class,
-        Atsmacode\PokerGame\PlayerHandler\PlayerHandler::class,
-        Atsmacode\PokerGame\Game\PotLimitHoldEm::class,
-        Atsmacode\PokerGame\Game\PotLimitOmaha::class,
-        Atsmacode\PokerGame\PokerGameConfigProvider::class,
+        \Atsmacode\PokerGame\HandStep\Showdown::class,
+        \Atsmacode\PokerGame\PlayerHandler\PlayerHandler::class,
+        \Atsmacode\PokerGame\Game\PotLimitHoldEm::class,
+        \Atsmacode\PokerGame\Game\PotLimitOmaha::class,
+        \Atsmacode\PokerGame\PokerGameConfigProvider::class,
     ],
     'factories' => [
-        Atsmacode\Framework\Database\ConnectionInterface::class
-            => Atsmacode\PokerGame\Database\DbalLiveFactory::class,
+        \Atsmacode\Framework\Database\ConnectionInterface::class
+            => \Atsmacode\PokerGame\Database\DbalLiveFactory::class,
         PDO::class
-            => Atsmacode\PokerGame\Database\PdoLiveFactory::class,
-        Atsmacode\PokerGame\GameData\GameData::class
-            => Atsmacode\PokerGame\GameData\GameDataFactory::class,
-        Atsmacode\PokerGame\GamePlay\GamePlay::class 
-            => Atsmacode\PokerGame\GamePlay\GamePlayFactory::class,
-        Atsmacode\PokerGame\GameState\GameState::class 
-            => Atsmacode\PokerGame\GameState\GameStateFactory::class,
-        Atsmacode\PokerGame\ActionHandler\ActionHandler::class
-            => Atsmacode\PokerGame\ActionHandler\ActionHandlerFactory::class,
-        Atsmacode\PokerGame\Dealer\PokerDealer::class
-            => Atsmacode\PokerGame\Dealer\PokerDealerFactory::class,
-        Atsmacode\PokerGame\Models\Street::class
-            => Atsmacode\Framework\Models\ModelFactory::class,
-        Atsmacode\PokerGame\Models\Table::class
-            => Atsmacode\Framework\Models\ModelFactory::class,
-        Atsmacode\PokerGame\Models\Hand::class
-            => Atsmacode\Framework\Models\ModelFactory::class,
-        Atsmacode\PokerGame\Models\Player::class
-            => Atsmacode\Framework\Models\ModelFactory::class,
-        Atsmacode\PokerGame\Models\TableSeat::class
-            => Atsmacode\Framework\Models\ModelFactory::class,
-        Atsmacode\PokerGame\Models\HandStreet::class
-            => Atsmacode\Framework\Models\ModelFactory::class,
-        Atsmacode\PokerGame\Models\PlayerAction::class
-            => Atsmacode\Framework\Models\ModelFactory::class,
-        Atsmacode\PokerGame\Models\Stack::class
-            => Atsmacode\Framework\Models\ModelFactory::class,
-        Atsmacode\PokerGame\Models\Pot::class
-            => Atsmacode\Framework\Models\ModelFactory::class,
-        Atsmacode\PokerGame\Models\PlayerActionLog::class
-            => Atsmacode\Framework\Models\ModelFactory::class,
-        Atsmacode\PokerGame\Models\WholeCard::class
-            => Atsmacode\Framework\Models\ModelFactory::class,
-        Atsmacode\PokerGame\Models\HandStreetCard::class
-            => Atsmacode\Framework\Models\ModelFactory::class,
-        Atsmacode\PokerGame\Models\HandType::class
-            => Atsmacode\Framework\Models\ModelFactory::class,
-        Atsmacode\PokerGame\PlayerHandler\PlayerHandler::class
-            => Atsmacode\PokerGame\PlayerHandler\PlayerHandlerFactory::class,
-        Atsmacode\PokerGame\BetHandler\BetHandler::class
-            => Atsmacode\PokerGame\BetHandler\BetHandlerFactory::class,
-        Atsmacode\PokerGame\PotHandler\PotHandler::class
-            => Atsmacode\PokerGame\PotHandler\PotHandlerFactory::class,
-        Atsmacode\PokerGame\HandStep\NewStreet::class
-            => Atsmacode\PokerGame\HandStep\NewStreetFactory::class,
-        Atsmacode\PokerGame\HandStep\Start::class
-            => Atsmacode\PokerGame\HandStep\StartFactory::class,
-        Atsmacode\PokerGame\Factory\PlayerActionFactory::class
-            => Atsmacode\PokerGame\Factory\PlayerActionFactoryFactory::class
-    ]
+            => \Atsmacode\PokerGame\Database\PdoLiveFactory::class,
+        \Atsmacode\PokerGame\GameData\GameData::class
+            => \Atsmacode\PokerGame\GameData\GameDataFactory::class,
+        \Atsmacode\PokerGame\GamePlay\GamePlay::class 
+            => \Atsmacode\PokerGame\GamePlay\GamePlayFactory::class,
+        \Atsmacode\PokerGame\GameState\GameState::class 
+            => \Atsmacode\PokerGame\GameState\GameStateFactory::class,
+        \Atsmacode\PokerGame\ActionHandler\ActionHandler::class
+            => \Atsmacode\PokerGame\ActionHandler\ActionHandlerFactory::class,
+        \Atsmacode\PokerGame\Dealer\PokerDealer::class
+            => \Atsmacode\PokerGame\Dealer\PokerDealerFactory::class,
+        \Atsmacode\PokerGame\Models\Street::class
+            => \Atsmacode\Framework\Models\ModelFactory::class,
+        \Atsmacode\PokerGame\Models\Table::class
+            => \Atsmacode\Framework\Models\ModelFactory::class,
+        \Atsmacode\PokerGame\Models\Hand::class
+            => \Atsmacode\Framework\Models\ModelFactory::class,
+        \Atsmacode\PokerGame\Models\Player::class
+            => \Atsmacode\Framework\Models\ModelFactory::class,
+        \Atsmacode\PokerGame\Models\TableSeat::class
+            => \Atsmacode\Framework\Models\ModelFactory::class,
+        \Atsmacode\PokerGame\Models\HandStreet::class
+            => \Atsmacode\Framework\Models\ModelFactory::class,
+        \Atsmacode\PokerGame\Models\PlayerAction::class
+            => \Atsmacode\Framework\Models\ModelFactory::class,
+        \Atsmacode\PokerGame\Models\Stack::class
+            => \Atsmacode\Framework\Models\ModelFactory::class,
+        \Atsmacode\PokerGame\Models\Pot::class
+            => \Atsmacode\Framework\Models\ModelFactory::class,
+        \Atsmacode\PokerGame\Models\PlayerActionLog::class
+            => \Atsmacode\Framework\Models\ModelFactory::class,
+        \Atsmacode\PokerGame\Models\WholeCard::class
+            => \Atsmacode\Framework\Models\ModelFactory::class,
+        \Atsmacode\PokerGame\Models\HandStreetCard::class
+            => \Atsmacode\Framework\Models\ModelFactory::class,
+        \Atsmacode\PokerGame\Models\HandType::class
+            => \Atsmacode\Framework\Models\ModelFactory::class,
+        \Atsmacode\PokerGame\PlayerHandler\PlayerHandler::class
+            => \Atsmacode\PokerGame\PlayerHandler\PlayerHandlerFactory::class,
+        \Atsmacode\PokerGame\BetHandler\BetHandler::class
+            => \Atsmacode\PokerGame\BetHandler\BetHandlerFactory::class,
+        \Atsmacode\PokerGame\PotHandler\PotHandler::class
+            => \Atsmacode\PokerGame\PotHandler\PotHandlerFactory::class,
+        \Atsmacode\PokerGame\HandStep\NewStreet::class
+            => \Atsmacode\PokerGame\HandStep\NewStreetFactory::class,
+        \Atsmacode\PokerGame\HandStep\Start::class
+            => \Atsmacode\PokerGame\HandStep\StartFactory::class,
+        \Atsmacode\PokerGame\Factory\PlayerActionFactory::class
+            => \Atsmacode\PokerGame\Factory\PlayerActionFactoryFactory::class
+    ],
 ];
