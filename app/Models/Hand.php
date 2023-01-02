87b8187c5a08e875a07066ad214780150baff041
Mark Ramage <mramage@hotmail.co.uk>
2023-01-02 11:57:21 +0000

    Add query builder to hand model getDealer, getComm cards & getPlayers

--- app/Models/Hand.php
+++ app/Models/Hand.php
@@ -82,27 +82,18 @@ class Hand extends Model
         }
     }
 
-    public function getDealer()
+    public function getDealer(): self
     {
-        $query = sprintf("
-            SELECT
-                *
-            FROM
-                player_actions AS pa
-            LEFT JOIN
-                table_seats AS ts ON pa.table_seat_id = ts.id
-            WHERE
-                pa.hand_id = :hand_id
-            AND
-                ts.is_dealer = 1
-        ");
-
         try {
-            $stmt = $this->connection->prepare($query);
-            $stmt->bindParam(':hand_id', $this->id);
-            
-            $results = $stmt->executeQuery();
-            $rows    = $results->fetchAllAssociative();
+            $queryBuilder = $this->connection->createQueryBuilder();
+            $queryBuilder
+                ->select('pa.*')
+                ->from('player_actions', 'pa')
+                ->leftJoin('pa', 'table_seats', 'ts', 'pa.table_seat_id = ts.id')
+                ->where('pa.hand_id = ' . $queryBuilder->createNamedParameter($this->id))
+                ->andWhere('ts.is_dealer = 1');
+
+            $rows = $queryBuilder->executeStatement() ? $queryBuilder->fetchAllAssociative() : [];
 
             $this->setModelProperties($rows);
 
@@ -114,89 +105,67 @@ class Hand extends Model
 
     public function getPlayers(int $handId): array
     {
-        $query = sprintf("
-            SELECT
-                ts.can_continue,
-                ts.is_dealer,
-                ts.player_id,
-                ts.table_id,
-                pa.bet_amount,
-                pa.active,
-                pa.has_acted,
-                pa.big_blind,
-                pa.small_blind,
-                pa.action_id,
-                pa.hand_id,
-                pa.hand_street_id,
-                pa.id AS player_action_id,
-                ts.id AS table_seat_id,
-                s.amount AS stack,
-                a.name AS actionName,
-                p.name AS playerName
-            FROM
-                table_seats AS ts
-            LEFT JOIN
-                player_actions AS pa ON ts.id = pa.table_seat_id
-            LEFT JOIN
-                players AS p ON pa.player_id = p.id
-            LEFT JOIN
-                stacks AS s ON pa.player_id = s.player_id AND ts.table_id = s.table_id
-            LEFT JOIN
-                actions AS a ON pa.action_id = a.id
-            WHERE
-                pa.hand_id = :hand_id
-            ORDER BY
-                ts.id ASC
-        ");
-
         try {
-            $stmt = $this->connection->prepare($query);
-            $stmt->bindParam(':hand_id', $handId);
-            
-            $results = $stmt->executeQuery();
+            $queryBuilder = $this->connection->createQueryBuilder();
+            $queryBuilder
+                ->select(
+                    'ts.can_continue',
+                    'ts.is_dealer',
+                    'ts.player_id',
+                    'ts.table_id',
+                    'pa.bet_amount',
+                    'pa.active',
+                    'pa.has_acted',
+                    'pa.big_blind',
+                    'pa.small_blind',
+                    'pa.action_id',
+                    'pa.hand_id',
+                    'pa.hand_street_id',
+                    'pa.id player_action_id',
+                    'ts.id table_seat_id',
+                    's.amount stack',
+                    'a.name actionName',
+                    'p.name playerName'
+                )
+                ->from('table_seats', 'ts')
+                ->leftJoin('ts', 'player_actions', 'pa', 'ts.id = pa.table_seat_id')
+                ->leftJoin('pa', 'players', 'p', 'pa.player_id = p.id')
+                ->leftJoin('pa', 'stacks', 's', 'pa.player_id = s.player_id AND ts.table_id = s.table_id')
+                ->leftJoin('pa', 'actions', 'a', 'pa.action_id = a.id')
+                ->where('pa.hand_id = ' . $queryBuilder->createNamedParameter($handId))
+                ->orderBy('ts.id', 'ASC');
 
-            return $results->fetchAllAssociative();
+            return $queryBuilder->executeStatement() ? $queryBuilder->fetchAllAssociative() : [];
         } catch(\Exception $e) {
             error_log(__METHOD__ . ': ' . $e->getMessage());
         }
     }
 
-    public function getCommunityCards(int $handId = null)
+    public function getCommunityCards(int $handId = null): array
     {
         $handId = $handId ?? $this->id;
-        $query  = sprintf("
-            SELECT
-                c.*, 
-                r.name AS 'rank',
-                r.abbreviation AS rankAbbreviation,
-                s.name AS suit,
-                s.abbreviation AS suitAbbreviation,
-                r.ranking AS ranking 
-            FROM
-                hand_street_cards AS hsc
-            LEFT JOIN
-                hand_streets AS hs ON hsc.hand_street_id = hs.id
-            LEFT JOIN
-                hands AS h ON hs.hand_id = h.id
-            LEFT JOIN
-                cards AS c ON hsc.card_id = c.id
-            LEFT OUTER JOIN 
-                ranks r ON c.rank_id = r.id
-            LEFT OUTER JOIN 
-                suits s ON c.suit_id = s.id
-            WHERE
-                h.id = :id
-            ORDER BY
-                hsc.id
-        ");
 
         try {
-            $stmt = $this->connection->prepare($query);
-            $stmt->bindParam(':id', $handId);
+            $queryBuilder = $this->connection->createQueryBuilder();
+            $queryBuilder
+                ->select(
+                    'c.*',
+                    'r.name rank',
+                    'r.abbreviation rankAbbreviation',
+                    's.name suit',
+                    's.abbreviation suitAbbreviation',
+                    'r.ranking ranking '
+                )
+                ->from('hand_street_cards', 'hsc')
+                ->leftJoin('hsc', 'hand_streets', 'hs', 'hsc.hand_street_id = hs.id')
+                ->leftJoin('hs', 'hands', 'h', 'hs.hand_id = h.id')
+                ->leftJoin('hsc', 'cards', 'c', 'hsc.card_id = c.id')
+                ->leftJoin('c', 'ranks', 'r', 'c.rank_id = r.id')
+                ->leftJoin('c', 'suits', 's', 'c.suit_id = s.id')
+                ->where('h.id = ' . $queryBuilder->createNamedParameter($handId))
+                ->orderBy('hsc.id', 'ASC');
 
-            $results = $stmt->executeQuery();
-
-            return $results->fetchAllAssociative();
+            return $queryBuilder->executeStatement() ? $queryBuilder->fetchAllAssociative() : [];
         } catch(\Exception $e) {
             error_log(__METHOD__ . ': ' . $e->getMessage());
         }
